a27581aa9250b8f7f4ee73961659a671f3b06b68
Renato <renatomarquesteles@gmail.com>
2024-01-05 21:01:40 -0300

    setup: fonts, theme, loading

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
+import { Loading } from '@components/Loading';
 import { Groups } from '@screens/Groups';
+import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar style="light" />
-      <Groups />
-    </>
+      {!fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
